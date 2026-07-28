29fe127f1eec2147015ef42a83a015833e0d649c
rayricabar5 <raymondashleyricabar5@gmail.com>
2026-07-28 23:38:13 +0800

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    name: "Task API",
+    version: "1.0",
+    endpoints: ["/tasks"]
+  })
+});
+
+app.get("health", (req, res) => {
+  res.json({status: "ok"});
 });
 
 app.listen(port, () => {
